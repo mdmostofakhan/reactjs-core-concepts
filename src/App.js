704520c3a6f975d8dfff8962bdb6f923f80890a8
mostofa khan <mdmostofakhan1972@gmail.com>
2023-03-22 22:10:48 +0600

    peson

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 5555;
-const singer = {name: 'Tasrif Khan', job: 'singer'}
-const products = {name: 'laptop', price: '43333'}
+const singers = [
+                {name: 'Tasrif Khan', job: 'singer'},
+                {name: 'Israfil Khan', job: 'developer'},
+                {name: 'Rubek Khan', job: 'business'},
+                {name: 'Mostofa Khan', job: 'market'},
+              
+              ]
 
 const singerStyle= {
    color: 'black',
@@ -11,32 +16,52 @@ const singerStyle= {
 }
 
 function App() {
+  const nayoks = ['Riaz Khan', 'Bapparaz', 'Israfil', 'Mostofa', 'Oli Knan', 'Mehedi', 'yachin']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>hello Dude. React. How are you?</h3>
-          <p style={singerStyle}> name: {444 + number}</p>
-          <p style={{color: 'black', backgroundColor: 'red'}}>name: {singer. name} {singer. job}</p>
-          <p style={singerStyle}>name: {products.name} {products. price}</p>
-        </div>
+      {
+        nayoks.map(nayok => <li>Name:{nayok}</li>)
+      }
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+
+      {
+        singers.map(singer => <Person name={singer.name}></Person> )
+      }
+{/* 
+    <Person name={nayoks[0]} nayka="purnima"></Person>
+    <Person name={nayoks[1]} nayka="popi"></Person>
+    <Person name={nayoks[2]} nayka="cheka"></Person> */}
+
+    <h1>New component, function</h1>
+    <p>rock mama React mama</p>
+    <Friend title="Israfil Chowdhury" job="Web devoloper"></Friend>
+    <Friend title="Mostofa Khan" job="Devoloper"></Friend>
+    <Friend title="i am so happy"></Friend>
     </div>
   );
+}
+
+
+function Person(props){
+  // console.log(props)
+  return (
+    <div className='person'>
+     <h1>{props.name}</h1>
+     <p>{props.nayka}</p>
+  </div>
+  )
+}
+
+function Friend(props){
+  return (
+     <div className='container'>
+        <h1>{props.title}</h1>
+        <p>{props.job}</p>
+     </div>
+  )
 }
 
 export default App;
