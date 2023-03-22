d0af13343960bdf15895ee3ef8cf47c8d5cce70f
mostofa khan <mdmostofakhan1972@gmail.com>
2023-03-22 19:22:50 +0600

    laptop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = {name: 'Tasrif Khan', job: 'singer'}
+const products = {name: 'laptop', price: '43333'}
+
+const singerStyle= {
+   color: 'black',
+   backgroundColor: 'yellow'
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,7 +18,11 @@ function App() {
         <h1>JSX</h1>
         <div className="container">
           <h3>hello Dude. React. How are you?</h3>
+          <p style={singerStyle}> name: {444 + number}</p>
+          <p style={{color: 'black', backgroundColor: 'red'}}>name: {singer. name} {singer. job}</p>
+          <p style={singerStyle}>name: {products.name} {products. price}</p>
         </div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
